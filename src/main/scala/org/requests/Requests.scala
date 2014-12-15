1996dbe2e445824f1499e3ea5aff67f242a1eb9a
Long Cao <longcao@gmail.com>
2014-12-15 01:10:48 -0500

    requests.get calls request, Requests object

--- src/main/scala/org/requests/Requests.scala
+++ src/main/scala/org/requests/Requests.scala
@@ -63,12 +63,58 @@ trait Requests {
     //cert
   ): Future[Response]
 
+  def get(
+    url: URL,
+    params: Map[String, String] = Map.empty,
+    data: Option[String] = None,
+    json: Option[String] = None,
+    headers: Seq[RequestHeader] = Seq.empty,
+    cookies: Seq[Cookie] = Seq.empty,
+    files: Map[String, File] = Map.empty,
+    //auth
+    timeout: Int,
+    allowRedirects: Boolean = true,
+    //proxies
+    //verify
+    stream: Boolean = false
+    //cert
+  ): Future[Response] = {
+    request(RequestMethod.Get, url, params, data, json, headers, cookies, files, timeout, allowRedirects, stream)
+  }
+
   def head: Future[Response]
-  def get: Future[Response]
   def post: Future[Response]
   def put: Future[Response]
   def patch: Future[Response]
   def delete: Future[Response]
 
   def codes: Map[String, Int]
+}
+
+object Requests extends Requests {
+  def request(
+    method: RequestMethod,
+    url: URL,
+    params: Map[String, String] = Map.empty,
+    data: Option[String] = None,
+    json: Option[String] = None,
+    headers: Seq[RequestHeader] = Seq.empty,
+    cookies: Seq[Cookie] = Seq.empty,
+    files: Map[String, File] = Map.empty,
+    //auth
+    timeout: Int,
+    allowRedirects: Boolean = true,
+    //proxies
+    //verify
+    stream: Boolean = false
+    //cert
+  ): Future[Response] = ???
+
+  def head: Future[Response] = ???
+  def post: Future[Response] = ???
+  def put: Future[Response] = ???
+  def patch: Future[Response] = ???
+  def delete: Future[Response] = ???
+
+  def codes: Map[String, Int] = ???
 }
