5691874bbbe82be5030616b1863e8b793b36ac2e
Long Cao <longcao@gmail.com>
2015-01-30 17:28:49 -0500

    use behavior stuff from ScalaTest, compare GET/HEAD headers test

--- src/test/scala/org/requests/HeadSpec.scala
+++ src/test/scala/org/requests/HeadSpec.scala
@@ -1,13 +1,24 @@
 package org.requests
 
 class HeadSpec extends RequestsSpec {
-  s"""head("$getUrl")""" should "return the correct response with no body" in {
+  behavior of s"""head("$getUrl")"""
+
+  it should "an OK with headers but no message body" in {
     val result = requests.head(url = getUrl)
 
     whenReady(result) { r =>
       r.status should === (org.requests.status.OK)
       r.headers should not be empty
       r.content shouldBe empty
+    }
+  }
+
+  it should "have the same headers as a GET request" in {
+    val getResult  = requests.get(url = getUrl)
+    val headResult = requests.head(url = getUrl)
+
+    whenReady(getResult.zip(headResult)) { case (r1, r2) =>
+      r1.headers should === (r2.headers)
     }
   }
 }
