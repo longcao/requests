db797257ef9dbf2c873c1f45281ccf1b5fdb01c8
Long Cao <longcao@gmail.com>
2015-02-05 13:09:59 -0500

    rm nondeterministic and problematic test

--- src/test/scala/org/requests/HeadSpec.scala
+++ src/test/scala/org/requests/HeadSpec.scala
@@ -12,13 +12,4 @@ class HeadSpec extends RequestsSpec {
       r.content shouldBe empty
     }
   }
-
-  it should "have the same headers as a GET request" in {
-    val getResult  = requests.get(url = getUrl)
-    val headResult = requests.head(url = getUrl)
-
-    whenReady(getResult.zip(headResult)) { case (r1, r2) =>
-      r1.headers should === (r2.headers)
-    }
-  }
 }
